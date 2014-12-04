d1ea72afec792ae0e4b90a06a4cd35fd85b5b8e8
Dessie K <desmond.kirrane@gmail.com>
2014-12-04 16:02:55 +0000

    fix yaml

--- web/env.sh
+++ web/env.sh
@@ -4,6 +4,9 @@
 MY_DB=`echo ${DB_PORT}`
 export MY_DB
 
+IP=`ip addr show eth0 | awk '/inet / {print $2}' | cut -d/ -f1`
+export IP
+
 # Execute the commands passed to this script
 # e.g. "./env.sh venv/bin/nosetests --with-xunit
 exec "$@"
